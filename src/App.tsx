0def832e73602a043c0fcb677cadf232a6d8b68b
handi priyono <handi.dev16@gmail.com>
2023-06-19 15:45:42 +0700

    feat: adjust app.tsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import SessionScreen from './Screens/Sessions/Sessions.screen';
+
+function App(): JSX.Element {
+  return <SessionScreen />;
+}
+
+export default App;
